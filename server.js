4a56f7bbece5ac29e2c90bc6ebd622132a908426
mutsambiwaDenver <mutsamu_aehit@students.vizja.pl>
2025-02-06 08:59:15 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+touch server.js
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
